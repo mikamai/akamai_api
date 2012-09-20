91c8bfaf826c6751de97ed8f1685db48da3da171
Nicola Racco <nicola@nicolaracco.com>
2012-09-20 11:04:25 +0200

    .

--- akamai_api/lib/akamai_api.rb
+++
@@ -1,9 +0,0 @@
-require 'savon'
-
-%w(version cp_code soap_body ccu_response ccu eccu_request).each do |file|
-  require File.expand_path "../akamai_api/#{file}", __FILE__
-end
-
-module AkamaiApi
-  def self.config; @config ||= { :auth => ['', ''] }; end
-end
--- akamai_api/lib/akamai_api/version.rb
+++
@@ -1,3 +0,0 @@
-module AkamaiApi
-  VERSION = "0.0.1"
-end
