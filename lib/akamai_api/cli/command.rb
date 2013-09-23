a73e56ad9cbefff795509c69ca6fbfbf37469240
Damian Fuentes <damianfuentes@livenation.com>
2013-09-23 12:45:22 -0700

    it was assuming that config file would always need to exist. I made it an option along with command line params and env variables

--- lib/akamai_api/cli/command.rb
+++ lib/akamai_api/cli/command.rb
@@ -8,15 +8,19 @@ module AkamaiApi
 
       no_tasks do
         def load_config
+          config = {}
           config_file = File.expand_path '~/.akamai_api.yml'
-          config = YAML::load_file(config_file).symbolize_keys
-          if options[:username] && options[:password]
-            config.merge! :auth => [options[:username], options[:password]]
-          elsif ENV['AKAMAI_USERNAME'] && ENV['AKAMAI_PASSWORD']
+          if File::exists?( config_file )
+            config = YAML::load_file(config_file).symbolize_keys
+          end
+          if ENV['AKAMAI_USERNAME'] && ENV['AKAMAI_PASSWORD']
             config.merge! :auth => [ENV['AKAMAI_USERNAME'], ENV['AKAMAI_PASSWORD']]
           end
+          if options[:username] && options[:password]
+            config.merge! :auth => [options[:username], options[:password]]
+          end
           if config[:auth].nil? || config[:auth].compact.blank?
-            raise "#{config_file} does not exist or doesn't contain auth info and you didn't specify username and password options"
+            raise "#{config_file} does not exist OR doesn't contain auth info OR you didn't export environment variables OR you didn't specify username and password on the command line"
           end
           AkamaiApi.config.merge! config
         end
