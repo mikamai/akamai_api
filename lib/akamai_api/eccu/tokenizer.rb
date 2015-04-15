61baa42e7a9e1a33a5e5d4e02db5b80e089aaa1b
oeN <diomede.tripicchio@gmail.com>
2015-04-15 10:45:54 +0200

    removed class for tokenizer_block first working tokenizer

--- lib/akamai_api/eccu/tokenizer.rb
+++ lib/akamai_api/eccu/tokenizer.rb
@@ -1,4 +1,4 @@
-require 'akamai_api/eccu/tokenizer_block'
+require 'ostruct'
 
 module AkamaiApi::ECCU
   # Class used to tokenize path used for invalidate cache
@@ -9,39 +9,98 @@ module AkamaiApi::ECCU
     attr_accessor :path, :splitted_path, :blocks
 
     SEPARATOR = '/'
-    # When the extension is present i use the sub_separator
+    # When the extension is present i use the ext_separator
     # to obtain the sub block
     #
     # ex: *.png => [*, png]
-    SUB_SEPARATOR = '.'
+    EXT_SEPARATOR = '.'
+
+    SPECIAL_TYPES = {
+      '*'        => :wildcard,
+      '**'       => :double_wildcard,
+    }
+    ALLOWED_TYPES = [:dir, :extension, :filename, :wildcard, :double_wildcard]
+    DEFAULT_TYPE = :dir
 
     def initialize p
       @path = p
-      @blocks = []
+      split_path
     end
 
     def extension_present?
-      if ! @path.include? "."
+      if ! @path.include? EXT_SEPARATOR
         false
       else
-        @path.rindex('.') > @path.rindex('/')
+        @path.rindex( EXT_SEPARATOR ) > @path.rindex( SEPARATOR )
       end
     end
 
-    # Use recursive method with self?
+    # if the last char in @path is the SEPARATOR indicate a dir
+    # otherwise a filename
+    def filename_present?
+      ! ( @path[-1,1] == SEPARATOR )
+    end
+
     def split_path
+      @blocks = []
       @splitted_path = @path.split(SEPARATOR)
       @splitted_path.each_with_index do |block, index|
-        if index == @splitted_path.size - 1
-          block.split(SUB_SEPARATOR).each do |sub_block|
-            @blocks << TokenizerBlock.new(sub_block)
-          end
-        else
-          @blocks << TokenizerBlock.new(block)
-        end
+        @blocks += create_blocks( block, (index == @splitted_path.size - 1) )
       end
 
       return @blocks
+    end
+
+    # create the object for a single block element
+    def single_block b, block_type = nil
+      block = OpenStruct.new
+      block.vallue = b
+      block.type = block_type || check_block_type( b )
+      block
+    end
+
+    # Create an array of blocks based on block string.
+    # Usually create an array with 1 element, but when the block is the last
+    # can return an array with multiple elements
+    #
+    # @param [String] block part of a path string (es. path: "foo/bar/*.png" blocks: [ foo, bar, *.png ])
+    # @param [Bool] last_block
+    #
+    # @return [Array] Array of blocks object
+    def create_blocks block, last_block = false
+      return [ single_block(block) ] if ! last_block
+      # last block
+      # extension before filename, becuse is more generic es. *.png
+      return extension_blocks( block ) if extension_present?
+      return [ single_block( block, ( filename_present? ? :filename : nil ) ) ]
+    end
+
+    # Create the array for the extension block
+    #
+    # @param [String] block es. *.png
+    #
+    # @return [Array] Array of blocks object
+    def extension_blocks block
+      extension_blocks = []
+      splitted_block = block.split(EXT_SEPARATOR)
+      splitted_block.each_with_index do |b, index|
+        t = splitted_block.size - 1 == index ? :extension : nil
+        extension_blocks << single_block(b, t)
+      end
+
+      if ! ( extension_blocks.map(&:type) & SPECIAL_TYPES.values ).empty?
+        return extension_blocks
+      else
+        return [ single_block(block, :filename) ]
+      end
+    end
+
+    def check_block_type block
+      SPECIAL_TYPES.each do |v, t|
+        return t if v == block
+      end
+
+      DEFAULT_TYPE
     end
 
   end
--- spec/lib/akamai_api/eccu/tokenizer_spec.rb
+++ spec/lib/akamai_api/eccu/tokenizer_spec.rb
@@ -4,6 +4,21 @@ module AkamaiApi::ECCU
   describe Tokenizer do
 
 
+    describe '.filename_present?' do
+      context 'return true' do
+        let(:eccu_tokenizer) { Tokenizer.new "foo/bar/file.png" }
+        it { expect(eccu_tokenizer.filename_present?).to be(true) }
+      end
+      context 'return false' do
+        let(:eccu_tokenizer) { Tokenizer.new "foo/bar/" }
+        it { expect(eccu_tokenizer.filename_present?).to be(false) }
+      end
+      context 'return true also without extension' do
+        let(:eccu_tokenizer) { Tokenizer.new "foo/bar" }
+        it { expect(eccu_tokenizer.filename_present?).to be(true) }
+      end
+    end
+
     describe '.extension_present?' do
       context 'return true' do
         let(:eccu_tokenizer) { Tokenizer.new "foo/bar/*.png" }
@@ -28,7 +43,24 @@ module AkamaiApi::ECCU
     describe '.split_path' do
       context 'return 4 blocks' do
         let(:eccu_tokenizer) { Tokenizer.new "foo/bar/*.png" }
-        it { expect(eccu_tokenizer.split_path.size).to equal 4 }
+        it { expect(eccu_tokenizer.blocks.size).to equal 4 }
+
+        it 'and all blocks have the correct type' do
+          expect( eccu_tokenizer.blocks[0].type ).to equal :dir
+          expect( eccu_tokenizer.blocks[1].type ).to equal :dir
+          expect( eccu_tokenizer.blocks[2].type ).to equal :wildcard
+          expect( eccu_tokenizer.blocks[3].type ).to equal :extension
+        end
+      end
+    end
+
+    describe '.check_block_type' do
+      subject{ Tokenizer.new "foo/bar/*.png" }
+      context 'first block is a :dir type' do
+        it { expect( subject.blocks.first.type ).to equal :dir }
+      end
+      context 'last block is a :extension type' do
+        it { expect( subject.blocks.last.type ).to equal :extension }
       end
     end
 
