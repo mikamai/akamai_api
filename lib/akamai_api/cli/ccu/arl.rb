52124a8a520d104480f1081f57d2545bcec5e804
jose.asuncion@gmail.com <jose.asuncion@gmail.com>
2015-05-04 10:58:52 -0700

    delegate polling to a method

--- lib/akamai_api/cli/ccu/arl.rb
+++ lib/akamai_api/cli/ccu/arl.rb
@@ -36,26 +36,29 @@ module AkamaiApi::CLI::CCU
         raise 'You should provide at least one valid URL' if arls.blank?
         load_config
         res = AkamaiApi::CCU.purge type, :arl, arls, :domain => options[:domain]
+        puts PurgeRenderer.new(res).render
 
         if options[:poll] == true && res.code == 201
-          puts PurgeRenderer.new(res).render
-          if res.time_to_wait
-            status = AkamaiApi::CCU.status res.uri
-            while status.completed_at.nil?
-              puts StatusRenderer.new(status).render
-              sleep 60
-              status = AkamaiApi::CCU.status res.uri
-            end
-
-            puts StatusRenderer.new(status).render
-          end
-        else
-          puts PurgeRenderer.new(res).render
+          poll_status res
         end
       rescue AkamaiApi::CCU::Error
         puts StatusRenderer.new($!).render_error
       rescue AkamaiApi::Unauthorized
         puts 'Your login credentials are invalid.'
+      end
+    end
+
+    no_commands do 
+      def poll_status purge_response
+        if purge_response.time_to_wait
+          status = AkamaiApi::CCU.status purge_response.uri
+          while status.completed_at.nil?
+            puts StatusRenderer.new(status).render
+            sleep 60
+            status = AkamaiApi::CCU.status purge_response.uri
+          end
+        end
+        puts StatusRenderer.new(status).render
       end
     end
   end
